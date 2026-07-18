a8648498c36888cb9497970d4e7f77da862a35d4
KislyjKisel <danilakisel@gmail.com>
2026-07-19 01:05:33 +0300

    remove bad import

--- src/LowDimLinAlg.lean
+++ src/LowDimLinAlg.lean
@@ -3,7 +3,6 @@ module
 public import LowDimLinAlg.Axis
 public import LowDimLinAlg.Matrix
 public import LowDimLinAlg.Quaternion
-public import LowDimLinAlg.Rotor
 public import LowDimLinAlg.Scalar
 public import LowDimLinAlg.Swizzling
 public import LowDimLinAlg.Vector
