35a95a3e43cdd514e7d29a0ad530d1013ae24a48
KislyjKisel <danilakisel@gmail.com>
2026-07-23 12:43:24 +0300

    V2.withZ, V3.withW

--- src/LowDimLinAlg/Vector/With.lean
+++ src/LowDimLinAlg/Vector/With.lean
@@ -20,8 +20,24 @@ run_cmd
   for dims in dimensionalities 2 4 do
     scalars.forM fun cx => do
       let sTy := cx.scalarType
-      let vTy := mkIdent <| Name.mkSimple <| cx.scalarPrefix ++ "Vector" ++ toString dims.size
+      let vTy := cx.structure <| "Vector" ++ toString dims.size
       elabCommand <| ← `(namespace $vTy)
+      if dims.size = 2 then
+        let v3Ty := cx.structure "Vector3"
+        elabCommand <| ← `(
+          /-- Creates a vector `⟨v.x, v.y, value⟩`. -/
+          @[inline]
+          def withZ (v : $vTy) (value : $sTy) : $v3Ty :=
+            .mk v.x v.y value
+        )
+      if dims.size = 3 then
+        let v4Ty := cx.structure "Vector4"
+        elabCommand <| ← `(
+          /-- Creates a vector `⟨v.x, v.y, v.z, value⟩`. -/
+          @[inline]
+          def withW (v : $vTy) (value : $sTy) : $v4Ty :=
+            .mk v.x v.y v.z value
+        )
       for dim in dims do
         let fId := mkIdent <| Name.mkSimple s!"with{dim.char.toUpper}"
         elabCommand <| ← `(
